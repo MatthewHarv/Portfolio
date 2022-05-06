e6e767709d798afd1149f52b48e738f3e92d2e4e
matthew <support@thepospeople.co.nz>
2022-05-06 18:08:26 +1200

    contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,102 +6,95 @@ export default class contact extends Component {
     return (
       <div className={style.outContact}>
         <div className={style.contactform}>
-          <h1 className={style.contactTitle}>Contact form</h1>
-
-          <div className="row">
+          <div className={style.content}>
+            <h1 className={style.contactTitle}>Lets get coffee</h1>
             <div className={style.contactContainer}>
-              <h3 className={style.infoTitle}>Contact Us</h3>
-              <p></p>
               <p className={style.infoDescription}>
-                If you need urgent assistance please call us on<br></br>
-                <strong>09 444 3903 / 0800 767 100 </strong>
-                <br></br>
-                Otherwise, please use the contact form and we will get back to
-                you.
+                I'm currently based in Dunedin, New Zealand. I happily work with
+                clients globally through Digital Mates.
               </p>
-              <br></br>
-              <h3 className={style.contactDetail}>Contact Details</h3>
-              <p></p>
-
-              <p className={style.infoContact}>support@thepospeople.co.nz</p>
-              <p className={style.infoContact}>sales@thepospeople.co.nz</p>
-              <p className={style.infoContact}>accounts@thepospeople.co.nz</p>
+              <p className={style.infoDescription}>
+                Aside from directly engineering I also consult and offer
+                technical guidance. It is important to ensure everyone involed
+                with a project is pointing in the same direction especially if
+                there are multiple external parties involved.
+              </p>
             </div>
-            <div className={style.formContainer}>
-              <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                netlify-honeypot="bot-field"
-                action="/success"
-              >
-                <input
-                  type="hidden"
-                  name="form-name"
-                  value="contact"
-                  aria-label="contactlabel"
-                />
-                <p className={style.hidden}>
-                  <label>
-                    Don’t fill this out if you're human:
-                    <input name="bot-field" aria-label="botlabel" />
-                  </label>
-                </p>
-                <div className="form-group">
-                  <label>
-                    Name
-                    <input
-                      name="name"
-                      type="text"
-                      aria-label="namelabel"
-                      className={style.input}
-                      required
-                    ></input>
-                  </label>
-                </div>
-                <div className="form-group">
-                  <label>
-                    Email
-                    <input
-                      name="email"
-                      type="email"
-                      aria-label="emaillabel"
-                      className={style.input}
-                      required
-                    ></input>
-                  </label>
-                </div>
-                <div className="form-group">
-                  <label>
-                    Phone/Mobile
-                    <input
-                      name="phone"
-                      type="text"
-                      aria-label="phonelabel"
-                      className={style.input}
-                      required
-                    ></input>
-                  </label>
-                </div>
-                <div className="form-group">
-                  <label>
-                    Message
-                    <textarea
-                      name="message"
-                      aria-label="messagelabel"
-                      className={style.input}
-                      rows="7"
-                      required
-                    ></textarea>
-                  </label>
-                </div>
-                <div className={style.formButton}>
-                  <button type="submit" className={style.inputButton}>
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
+          </div>
+          <div className={style.formContainer}>
+            <form
+              name="contact"
+              method="POST"
+              data-netlify="true"
+              netlify-honeypot="bot-field"
+              action="/success"
+            >
+              <input
+                type="hidden"
+                name="form-name"
+                value="contact"
+                aria-label="contactlabel"
+              />
+              <p className={style.hidden}>
+                <label>
+                  Don’t fill this out if you're human:
+                  <input name="bot-field" aria-label="botlabel" />
+                </label>
+              </p>
+              <div className="form-group">
+                <label className={style.labelContainer}>
+                  <div>Name</div>
+                  <input
+                    name="name"
+                    type="text"
+                    aria-label="namelabel"
+                    className={style.input}
+                    required
+                  ></input>
+                </label>
+              </div>
+              <div className="form-group">
+                <label>
+                  Email
+                  <input
+                    name="email"
+                    type="email"
+                    aria-label="emaillabel"
+                    className={style.input}
+                    required
+                  ></input>
+                </label>
+              </div>
+              <div className="form-group">
+                <label>
+                  Phone/Mobile
+                  <input
+                    name="phone"
+                    type="text"
+                    aria-label="phonelabel"
+                    className={style.input}
+                    required
+                  ></input>
+                </label>
+              </div>
+              <div className="form-group">
+                <label>
+                  Message
+                  <textarea
+                    name="message"
+                    aria-label="messagelabel"
+                    className={style.input}
+                    rows="7"
+                    required
+                  ></textarea>
+                </label>
+              </div>
+              <div className={style.formButton}>
+                <button type="submit" className={style.inputButton}>
+                  Send
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
