993d9b07139f4ae41f602356f0696d29a38c8a52
Matthew <matthewjharv@gmail.com>
2022-05-11 13:38:16 +1200

    add animation

--- src/components/about.js
+++ src/components/about.js
@@ -1,51 +1,93 @@
-import React, { Component } from "react"
+import React from "react"
 import * as style from "../components/css/about.module.scss"
 import { StaticImage } from "gatsby-plugin-image"
+import { useInView } from "react-intersection-observer"
 
-export default class about extends Component {
-  render() {
-    return (
-      <div className={style.container}>
-        <h2 className={style.title}>About Me</h2>
-        <div className={style.imageContainer}>
-          <StaticImage
-            className={style.siteimage}
-            src="../images/me.png"
-            alt="Matthew"
-            formats={["AUTO", "WEBP"]}
-          />
-        </div>
-        <p className={style.description}>
-          My name is Matthew Harvey. I am a graduate from the University Of
-          Otago and an enthusiast of programming and 2 wheels!
-        </p>
-        <p className={style.description}>
-          I have gained a variety of experience throughout my career, from
-          working forklifts in warehouses, to helping customers over the phone
-          in a technician role, to developing a companies website and designing
-          it to their liking.
-        </p>
-        <p className={style.description}>
-          I have a passion for web development and looking to improve my skills
-          and portfolio in the future. I currently build my websites using
-          GatsbyJS and enjoy the frontend development and designing of websites.
-        </p>
-        <p className={style.description}>
-          In the future, I would like to become a more rounded full stack
-          developer with experience on a variety of web frameworks.
-        </p>
-        <p className={style.description}>
-          All my code is freely available on my{" "}
-          <a
-            className={style.gitlink}
-            href="https://github.com/MatthewHarv"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className={style.github}>github.</span>{" "}
-          </a>
-        </p>
+export default function About() {
+  const { ref, inView: descriptionIsVisible } = useInView()
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.title}>About Me</h2>
+      <div className={style.imageContainer}>
+        <StaticImage
+          className={style.siteimage}
+          src="../images/me.png"
+          alt="Matthew"
+          formats={["AUTO", "WEBP"]}
+        />
       </div>
-    )
-  }
+      <p
+        ref={ref}
+        className={`${style.description} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        My name is Matthew Harvey. I am a graduate from the University Of Otago
+        and an enthusiast of programming and 2 wheels!
+      </p>
+      <p
+        ref={ref}
+        className={`${style.description} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        I have gained a variety of experience throughout my career, from working
+        forklifts in warehouses, to helping customers over the phone in a
+        technician role, to developing a companies website and designing it to
+        their liking.
+      </p>
+      <p
+        ref={ref}
+        className={`${style.description} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        I have a passion for web development and looking to improve my skills
+        and portfolio in the future. I currently build my websites using
+        GatsbyJS and enjoy the frontend development and designing of websites.
+      </p>
+      <p
+        ref={ref}
+        className={`${style.description} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        In the future, I would like to become a more rounded full stack
+        developer with experience on a variety of web frameworks.
+      </p>
+      <p
+        className={`${style.description} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        All my code is freely available on my{" "}
+        <a
+          className={style.gitlink}
+          href="https://github.com/MatthewHarv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className={style.github}>github.</span>{" "}
+        </a>
+      </p>
+    </div>
+  )
 }
+
+/*
+
+
+ const myRef = useRef()
+  const [myElementIsVisible, setMyElementIsVisible] = useState()
+  console.log("myElementIsVisible", myElementIsVisible)
+  useEffect(() => {
+    console.log("myRef", myRef.current)
+    const observer = new IntersectionObserver(entries => {
+      const entry = entries[0]
+      setMyElementIsVisible(entry.isIntersecting)
+    })
+    observer.observe(myRef.current)
+  }, [])
+
+*/
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,119 +1,109 @@
-import React, { Component } from "react"
+import React from "react"
 import * as style from "../components/css/contact.module.scss"
+import { useInView } from "react-intersection-observer"
 
-export default class contact extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isToggleOn: false,
-    }
+export default function Contact() {
+  const { ref, inView: descriptionIsVisible } = useInView()
 
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    console.log("helloo world!")
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-    }))
-  }
-
-  render() {
-    return (
-      <div className={style.outContact}>
-        <div className={style.contactform}>
+  return (
+    <div className={style.outContact}>
+      <div
+        ref={ref}
+        className={`${style.contactform} ${
+          descriptionIsVisible ? style.fadein : ""
+        }`}
+      >
+        <div>
+          <h1 className={style.contactTitle}>Contact Me</h1>
           <div>
-            <h1 className={style.contactTitle}>Contact Me</h1>
-            <div>
-              <p className={style.infoDescription}>
-                I'm currently based in Auckland, New Zealand.
-              </p>
-              <p className={style.infoDescription}>
-                I am currently searching for the next step in my programming
-                career. If you want to discuss all things web development or job
-                opportunities, please leave me a message and I will get back to
-                you as soon as I can.
-              </p>
-            </div>
-          </div>
-          <div>
-            <form
-              name="contact"
-              method="POST"
-              data-netlify="true"
-              netlify-honeypot="bot-field"
-              action="/success"
-            >
-              <input
-                type="hidden"
-                name="form-name"
-                value="contact"
-                aria-label="contactlabel"
-              />
-              <p className={style.hidden}>
-                <label>
-                  Don’t fill this out if you're human:
-                  <input name="bot-field" aria-label="botlabel" />
-                </label>
-              </p>
-              <div className="form-group">
-                <label className={style.labelContainer}>
-                  <input
-                    name="name"
-                    placeholder="Name"
-                    type="text"
-                    aria-label="namelabel"
-                    className={style.input}
-                    required
-                  ></input>
-                </label>
-              </div>
-              <div className="form-group">
-                <label>
-                  <input
-                    placeholder="Email"
-                    name="email"
-                    type="email"
-                    aria-label="emaillabel"
-                    className={style.input}
-                    required
-                  ></input>
-                </label>
-              </div>
-              <div className="form-group">
-                <label>
-                  <input
-                    name="phone"
-                    placeholder="Phone"
-                    type="text"
-                    aria-label="phonelabel"
-                    className={style.input}
-                    required
-                  ></input>
-                </label>
-              </div>
-              <div className="form-group">
-                <label>
-                  <textarea
-                    name="message"
-                    placeholder="Message"
-                    aria-label="messagelabel"
-                    className={style.inputMessage}
-                    rows="7"
-                    required
-                  ></textarea>
-                </label>
-              </div>
-
-              <div className={style.formButton}>
-                <button type="submit" className={style.inputButton}>
-                  Send
-                </button>
-              </div>
-            </form>
+            <p className={style.infoDescription}>
+              I'm currently based in Auckland, New Zealand.
+            </p>
+            <p className={style.infoDescription}>
+              I am currently searching for the next step in my programming
+              career. If you want to discuss all things web development or job
+              opportunities, please leave me a message and I will get back to
+              you as soon as I can.
+            </p>
           </div>
         </div>
+        <div>
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+            action="/success"
+          >
+            <input
+              type="hidden"
+              name="form-name"
+              value="contact"
+              aria-label="contactlabel"
+            />
+            <p className={style.hidden}>
+              <label>
+                Don’t fill this out if you're human:
+                <input name="bot-field" aria-label="botlabel" />
+              </label>
+            </p>
+            <div className="form-group">
+              <label className={style.labelContainer}>
+                <input
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                  aria-label="namelabel"
+                  className={style.input}
+                  required
+                ></input>
+              </label>
+            </div>
+            <div className="form-group">
+              <label>
+                <input
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  aria-label="emaillabel"
+                  className={style.input}
+                  required
+                ></input>
+              </label>
+            </div>
+            <div className="form-group">
+              <label>
+                <input
+                  name="phone"
+                  placeholder="Phone"
+                  type="text"
+                  aria-label="phonelabel"
+                  className={style.input}
+                  required
+                ></input>
+              </label>
+            </div>
+            <div className="form-group">
+              <label>
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  aria-label="messagelabel"
+                  className={style.inputMessage}
+                  rows="7"
+                  required
+                ></textarea>
+              </label>
+            </div>
+
+            <div className={style.formButton}>
+              <button type="submit" className={style.inputButton}>
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
